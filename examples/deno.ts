e0599b616ba877a43d4d8cf07db39efdeb11a87c
Patrick Smith <pgwsmith@gmail.com>
2020-12-04 09:16:06 +1100

    Add deno example

---
+++ examples/deno.ts
@@ -0,0 +1,25 @@
+import { parse, mustEnd } from 'https://unpkg.com/parcook@0.1.4?module';
+
+function* Digit() {
+  const [digit]: [string] = yield /^\d+/;
+  const value = parseInt(digit, 10);
+  if (value < 0 || value > 255) {
+    return new Error(`Digit must be between 0 and 255, was ${value}`);
+  }
+  return value;
+}
+
+function* IPAddress() {
+  const first = yield Digit;
+  yield '.';
+  const second = yield Digit;
+  yield '.';
+  const third = yield Digit;
+  yield '.';
+  const fourth = yield Digit;
+  yield mustEnd;
+  return [first, second, third, fourth];
+}
+
+const result = parse('1.2.3.4', IPAddress());
+console.log(result);
