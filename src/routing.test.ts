72a2387724a19100cd66a3c085c20a95d267f631
Patrick Smith <pgwsmith@gmail.com>
2022-01-28 10:32:37 +1100

    Move routing test into its own file

---
+++ src/routing.test.ts
@@ -0,0 +1,90 @@
+import { parse, mustEnd } from './index';
+
+describe('Router', () => {
+    type Route =
+      | { type: 'home' }
+      | { type: 'about' }
+      | { type: 'albums' }
+      | { type: 'album'; id: string }
+      | { type: 'albumArt'; id: string };
+
+    function* Home() {
+      yield '/';
+      yield mustEnd;
+      return { type: 'home' } as Route;
+    }
+
+    function* About() {
+      yield '/about';
+      yield mustEnd;
+      return { type: 'about' } as Route;
+    }
+
+    const Albums = {
+      *List() {
+        yield '/albums';
+        yield mustEnd;
+        return { type: 'albums' } as Route;
+      },
+      *ItemPrefix() {
+        yield '/albums/';
+        const [id]: [string] = yield /^\d+/;
+        return { id };
+      },
+      *Item() {
+        const { id }: { id: string } = yield Albums.ItemPrefix;
+        yield mustEnd;
+        return { type: 'album', id } as Route;
+      },
+      *ItemArt() {
+        const { id }: { id: string } = yield Albums.ItemPrefix;
+        yield '/art';
+        yield mustEnd;
+        return { type: 'albumArt', id } as Route;
+      },
+    };
+
+    function* AlbumRoutes() {
+      return yield [Albums.List, Albums.Item, Albums.ItemArt];
+    }
+
+    function* Route() {
+      return yield [Home, About, AlbumRoutes];
+    }
+
+    it('works with home', () => {
+      expect(parse('/', Route())).toEqual({
+        success: true,
+        result: { type: 'home' },
+        remaining: '',
+      });
+    });
+    it('works with about', () => {
+      expect(parse('/about', Route())).toEqual({
+        success: true,
+        result: { type: 'about' },
+        remaining: '',
+      });
+    });
+    it('works with albums', () => {
+      expect(parse('/albums', Route())).toEqual({
+        success: true,
+        result: { type: 'albums' },
+        remaining: '',
+      });
+    });
+    it('works with album for id', () => {
+      expect(parse('/albums/42', Route())).toEqual({
+        success: true,
+        result: { type: 'album', id: '42' },
+        remaining: '',
+      });
+    });
+    it('works with album art for id', () => {
+      expect(parse('/albums/42/art', Route())).toEqual({
+        success: true,
+        result: { type: 'albumArt', id: '42' },
+        remaining: '',
+      });
+    });
+  });
