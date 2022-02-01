0c4f14c13f8c2f9baf9f4a70970a8fb3cd715974
Patrick Smith <pgwsmith@gmail.com>
2022-02-01 11:42:10 +1100

    Support reversing a route with nested prefix

--- src/routing.test.ts
+++ src/routing.test.ts
@@ -123,15 +123,38 @@ describe("Router reversal", () => {
     return { type: "album", id };
   }
 
-  function* Route() {
-    return yield [Home, About];
+  function* BlogPrefix() {
+    yield "/blog/";
   }
 
-  it("works", () => {
+  function* BlogArticle() {
+    yield BlogPrefix;
+    const [slug]: [string] = yield /^.+/;
+    return { type: "blogArticle", slug };
+  }
+
+  function* Route() {
+    return yield [Home, About, Terms];
+  }
+
+  it("works with single route definition", () => {
     expect(reverse({ type: "home" }, Home())).toEqual("/");
     expect(reverse({ type: "about" }, About())).toEqual("/about");
     expect(reverse({ type: "terms" }, Terms())).toEqual("/legal/terms");
+  });
+
+  it("works with single route definition with param", () => {
     expect(reverse({ type: "album", id: "123" }, AlbumItem())).toEqual("/albums/123");
+  })
+
+  it("works with nested routes", () => {
+    expect(reverse({ type: "home" }, Route())).toEqual("/");
+    expect(reverse({ type: "about" }, Route())).toEqual("/about");
+    expect(reverse({ type: "terms" }, Route())).toEqual("/legal/terms");
+  });
+
+  it("works with routes with nested prefix", () => {
+    expect(reverse({ type: "blogArticle", slug: "hello-world" }, BlogArticle())).toEqual("/blog/hello-world");
   });
 });
 
@@ -139,6 +162,18 @@ function reverse<Result = void>(
   needle: {},
   iterable: ParseGenerator<Result>,
 ): string | null {
+  const result =  reverseInner(needle, iterable);
+  if (result !== null && result.type === 'done') {
+    return result.components.join('');
+  }
+
+  return null;
+}
+
+function reverseInner<Result = void>(
+  needle: {},
+  iterable: ParseGenerator<Result>,
+): { type: 'done' | 'prefix'; components: ReadonlyArray<string> } | null {
   let reply: unknown | undefined;
 
   const expectedKeys = Object.keys(needle);
@@ -146,7 +181,7 @@ function reverse<Result = void>(
     throw new Error("Expected object must have keys.");
   }
   const iterator = iterable[Symbol.iterator]();
-  const components: Array<string | RegExp> = [];
+  const components: Array<string> = [];
   const regexpMap = new Map<Symbol, { regexp: RegExp; index: number }>();
 
   while (true) {
@@ -157,6 +192,10 @@ function reverse<Result = void>(
       }
 
       const result = next.value;
+      if (result == null) {
+        return { type: 'prefix', components: Object.freeze(components) };
+      }
+
       const resultKeys = new Set(Object.keys(result));
       if (
         expectedKeys.length === resultKeys.size &&
@@ -178,7 +217,7 @@ function reverse<Result = void>(
           return result[key] === needle[key];
         })
       ) {
-        return components.join('');
+        return { type: 'done', components: Object.freeze(components) };
       } else {
         return null;
       }
@@ -191,20 +230,29 @@ function reverse<Result = void>(
         : [yielded];
 
     for (const choice of choices) {
+      reply = undefined;
+
       if (typeof choice === "string") {
         components.push(choice);
         reply = choice;
+        break; // Assume first string is the canonical version.
       } else if (choice instanceof RegExp) {
         const index = components.length;
-        components.push('???'); // This will be replaced later using the index.
+        components.push(''); // This will be replaced later using the index.
+        // components.push('???'); // This will be replaced later using the index.
         const s = Symbol();
         regexpMap.set(s, { regexp: choice, index });
         reply = [s];
       } else if (choice instanceof Function) {
-          // TODO: call
+        const result = reverseInner(needle, choice());
+        if (result != null) {
+          if (result.type === 'done') {
+            return { type: 'done', components: Object.freeze(components.concat(result.components)) };
+          } else {
+            components.push(...result.components);
+          }
+        }
       }
     }
-
-    // return components.join("");
   }
 }
