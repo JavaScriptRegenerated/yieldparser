f9f14aac83f024517f83277e73c1f617f379d8c3
Patrick Smith <pgwsmith@gmail.com>
2022-02-01 11:49:46 +1100

    Better tests

--- src/routing.test.ts
+++ src/routing.test.ts
@@ -124,36 +124,68 @@ describe("Router inversion", () => {
   }
 
   function* BlogPrefix() {
-    yield "/blog/";
+    yield "/blog";
+  }
+
+  function* BlogHome() {
+    yield BlogPrefix;
+    yield mustEnd;
+    return { type: "blog" };
   }
 
   function* BlogArticle() {
     yield BlogPrefix;
+    yield "/";
     const [slug]: [string] = yield /^.+/;
     return { type: "blogArticle", slug };
   }
 
-  function* Route() {
+  function* BlogRoutes() {
+    return yield [BlogHome, BlogArticle];
+  }
+
+  function* Routes() {
     return yield [Home, About, Terms];
+  }
+
+  function* DoubleNested() {
+    return yield [BlogRoutes, Routes];
   }
 
   it("works with single route definition", () => {
     expect(invert({ type: "home" }, Home())).toEqual("/");
     expect(invert({ type: "about" }, About())).toEqual("/about");
     expect(invert({ type: "terms" }, Terms())).toEqual("/legal/terms");
+    expect(invert({ type: "BLAH" }, Terms())).toBeNull();
   });
 
   it("works with single route definition with param", () => {
     expect(invert({ type: "album", id: "123" }, AlbumItem())).toEqual("/albums/123");
+    expect(invert({ type: "album", id: "678" }, AlbumItem())).toEqual("/albums/678");
+    expect(invert({ type: "album", id: "abc" }, AlbumItem())).toBeNull();
+    expect(invert({ type: "BLAH", id: "123" }, AlbumItem())).toBeNull();
   })
 
   it("works with nested routes", () => {
-    expect(invert({ type: "home" }, Route())).toEqual("/");
-    expect(invert({ type: "about" }, Route())).toEqual("/about");
-    expect(invert({ type: "terms" }, Route())).toEqual("/legal/terms");
+    expect(invert({ type: "home" }, Routes())).toEqual("/");
+    expect(invert({ type: "about" }, Routes())).toEqual("/about");
+    expect(invert({ type: "terms" }, Routes())).toEqual("/legal/terms");
+    expect(invert({ type: "BLAH" }, Routes())).toBeNull();
   });
 
   it("works with routes with nested prefix", () => {
+    expect(invert({ type: "blog" }, BlogHome())).toEqual("/blog");
     expect(invert({ type: "blogArticle", slug: "hello-world" }, BlogArticle())).toEqual("/blog/hello-world");
+
+    expect(invert({ type: "blog" }, BlogRoutes())).toEqual("/blog");
+    expect(invert({ type: "blogArticle", slug: "hello-world" }, BlogRoutes())).toEqual("/blog/hello-world");
+    expect(invert({ type: "BLAH" }, BlogRoutes())).toBeNull();
+  });
+
+  it("all works with double nested routes", () => {
+    expect(invert({ type: "home" }, DoubleNested())).toEqual("/");
+    expect(invert({ type: "blog" }, DoubleNested())).toEqual("/blog");
+    expect(invert({ type: "blogArticle", slug: "hello-world" }, DoubleNested())).toEqual("/blog/hello-world");
+    expect(invert({ type: "BLAH" }, DoubleNested())).toBeNull();
   });
 });
